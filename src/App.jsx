781cd2f60293762eb86d6bc952523eccddba74ab
Tawanda Nduna Mashoko <mashokotawanda61@gmail.com>
2025-12-06 04:17:04 +0100

    finilize portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Dock, Navbar, Welcome,} from "#components";
+import { Dock, Home, Navbar, Welcome,} from "#components";
 import { Contact, Finder, Resume, Safari, Terminal, Text } from "./windows";
 import { gsap } from "gsap";
 import { Draggable } from "gsap/Draggable";
@@ -17,6 +17,7 @@ const App = () => {
       <Finder />
       <Text />
       <Contact />
+      <Home />
     </main>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "#components/navbar.jsx";
 import Welcome from "#components/welcome.jsx";
 import Dock from "#components/dock.jsx";
 import WindowControls from "./WindowControls";
+import Home from "./Home";
 
-export { Navbar, Welcome, Dock, WindowControls };
+export { Navbar, Welcome, Dock, WindowControls, Home };
